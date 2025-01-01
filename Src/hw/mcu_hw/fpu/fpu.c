c98e8a9f7d887b3d0b24474df81ae6f5218c93ab
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-01-01 17:41:51 +0100

    fpu: add implementation

--- Src/hw/mcu_hw/fpu/fpu.c
+++ Src/hw/mcu_hw/fpu/fpu.c
@@ -4,5 +4,10 @@
 
 void fpu_init(void)
 {
-    
+    /* Enable Coprocessors CP10 an CP11 in privileged and unprivileged mode (full access) */
+    SCB -> CPACR = 0x0F << 20;
+
+    /* Make sure that changes got applied */
+    __DSB();
+    __ISB();
 }
