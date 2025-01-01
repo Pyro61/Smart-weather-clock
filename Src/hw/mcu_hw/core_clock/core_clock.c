82b5fe80d486085e28a65e7980efd19d7299269a
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-01-01 17:02:57 +0100

    core clock: add implementation

--- Src/hw/mcu_hw/core_clock/core_clock.c
+++ Src/hw/mcu_hw/core_clock/core_clock.c
@@ -3,24 +3,34 @@
 
 
 /* Current clock freq */
-uint32_t clock_freq = 12000000;
+uint32_t clock_freq = 16000000;
 
 
-/* HSE - 48MHz */
+/* HSI16 - 48MHz */
 void core_clock_config(void)
 {
     /* Flash access delay */
-	FLASH -> ACR |= FLASH_ACR_LATENCY_0;
-	while (!(FLASH -> ACR & FLASH_ACR_LATENCY));
+	FLASH->ACR &= ~FLASH_ACR_LATENCY;    /* Reset current latency setting */
+    FLASH->ACR |= FLASH_ACR_LATENCY_1WS; /* Set required wait states - 1WS */
+	while ((FLASH -> ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_1WS); /* Wait for register update */
 
-	/* Enable HSE clock */
-	RCC -> CR |= RCC_CR_HSEON;
-	while (!(RCC -> CR & RCC_CR_HSERDY));
+	/* Disable HSI16 in low power modes */
+	RCC -> CR &= ~RCC_CR_HSIKERON;
 
-	/* Select SYSCLK clock - HSE */
-	RCC -> CFGR |= RCC_CFGR_SW_0;
+	/* Configure PLL 
+	 * Output frequency = entry clock * (PLLN/PLLM) / PLLR */
+	RCC -> PLLCFGR = RCC_PLLCFGR_PLLSRC_1; /* Select HSI16 as entry clock source */
+	RCC -> PLLCFGR |= 12 << 8; /* Main PLL multiplicator factor - PLLN = 12 */
+	RCC -> PLLCFGR |= 0 << 4; /* Main PLL division factor - PLLM = 1 */
+	RCC -> PLLCFGR |= 1 << 25; /* PLL "R" clock division factor - PLLR = 4 */
+	RCC -> PLLCFGR |= RCC_PLLCFGR_PLLREN; /* Enable PLL "R" clock */
+	RCC -> CR |= RCC_CR_PLLON; /* Enable PLL */
+	while (!(RCC->CR & RCC_CR_PLLRDY)); /* Wait for PLL to be ready */
 
-	/* Disable HSI */
-	RCC -> CR &= ~RCC_CR_HSION;
+	/* Select system clock - PLL */
+	RCC -> CFGR |= RCC_CFGR_SW;
+	/* Wait for switch to PLL */
+	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
 	clock_freq = 48000000;
 }
--- Src/hw/mcu_hw/core_clock/core_clock.h
+++ Src/hw/mcu_hw/core_clock/core_clock.h
@@ -1,2 +1,10 @@
 #pragma once
 
+#include "stdint.h"
+
+
+/* Current clock frequency */
+extern uint32_t clock_freq;
+
+/* Core clock config - PPL (HSI16) 48Mhz */
+void core_clock_config(void);
