b6e7d552fd736df343ce1440f4488867fa514c57
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-02-02 18:54:41 +0100

    aht15: add implementation
    -init
    -measuring trigger
    -get humidity
    -get temperature

--- Src/hw/external_hw/aht15/aht15.h
+++ Src/hw/external_hw/aht15/aht15.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "stdint.h"
+
+
+void aht15_start_measuring(void);
+void aht15_init(void);
+int32_t aht15_get_humidity(void);
+int32_t aht15_get_temp(void);
+void aht15_read_data(void);
--- Src/hw/external_hw/external_hw_init.c
+++ Src/hw/external_hw/external_hw_init.c
@@ -1,7 +1,9 @@
 #include "external_hw_init.h"
 #include "LPS25HB/LPS25HB.h"
+#include "aht15/aht15.h"
 
 void external_hw_init(void)
 {
     LPS25HB_init();
+    aht15_init();
 }
