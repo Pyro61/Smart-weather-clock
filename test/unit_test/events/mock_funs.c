6e6afe83fdb4fb2d0663c31d064c2d2e26cd277f
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-02-04 21:37:01 +0100

    unit_test: fix test fail is events module
    create more mock functions to pass ReachMaxSubscribersThenSubscribe1MoreEventAndCallEvent test

--- test/unit_test/events/mock_funs.c
+++ test/unit_test/events/mock_funs.c
@@ -9,6 +9,23 @@ void mock_fun4(void) {}
 void mock_fun5(void) {}
 void mock_fun6(void) {}
 void mock_fun7(void) {}
+void mock_fun8(void) {}
+void mock_fun9(void) {}
+void mock_fun10(void) {}
+void mock_fun11(void) {}
+void mock_fun12(void) {}
+void mock_fun13(void) {}
+void mock_fun14(void) {}
+void mock_fun15(void) {}
+void mock_fun16(void) {}
+void mock_fun17(void) {}
+void mock_fun18(void) {}
+void mock_fun19(void) {}
+void mock_fun20(void) {}
+void mock_fun21(void) {}
+void mock_fun22(void) {}
+void mock_fun23(void) {}
+void mock_fun24(void) {}
 
 static callback_t mock_funs_array[EVENTS_MAX_SUBSCRIBERS] = 
 {
@@ -20,6 +37,23 @@ static callback_t mock_funs_array[EVENTS_MAX_SUBSCRIBERS] =
     mock_fun5,
     mock_fun6,
     mock_fun7,
+    mock_fun8,
+    mock_fun9,
+    mock_fun10,
+    mock_fun11,
+    mock_fun12,
+    mock_fun13,
+    mock_fun14,
+    mock_fun15,
+    mock_fun16,
+    mock_fun17,
+    mock_fun18,
+    mock_fun19,
+    mock_fun20,
+    mock_fun21,
+    mock_fun22,
+    mock_fun23,
+    mock_fun24,
 };
 
 callback_t get_mock_fun_addr(uint8_t n)
