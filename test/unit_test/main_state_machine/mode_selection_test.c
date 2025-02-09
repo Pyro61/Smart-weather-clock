6b75a78a6e1d901485d8cd6412d5a9838431d624
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-02-09 15:29:16 +0100

    unit test: add unit test configuration for main state machine
    every state will be tested in separation, no tests for all state machine

---
+++ test/unit_test/main_state_machine/mode_selection_test.c
@@ -0,0 +1,20 @@
+#include "unity/fixture/unity_fixture.h"
+
+#include "assert.h"
+
+TEST_GROUP(mode_selection);
+
+TEST_SETUP(mode_selection)
+{
+    /* Init before every test */
+}
+
+TEST_TEAR_DOWN(mode_selection)
+{
+    /* Cleanup after every test */
+}
+
+TEST(mode_selection, FirstTest)
+{
+    TEST_FAIL_MESSAGE("Implement your test!");
+}
