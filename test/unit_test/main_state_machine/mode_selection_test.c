272cbc59ccfa65671adab4590e4db9a05107e975
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-02-09 17:45:46 +0100

    unit test: main_state_machine: mode selection: refactoring test code

--- test/unit_test/main_state_machine/mode_selection_test.c
+++ test/unit_test/main_state_machine/mode_selection_test.c
@@ -6,58 +6,109 @@
 #include "display_mock.h"
 #include <string.h>
 
+
+#define CLEARED_MESSAGE             "\0"
+#define SELECTED_MODE_1_MESSAGE     "> 1. weather indoor\n  2. weather outdoor\n  3. set time\n  4. set alarm"
+
+#define MODE_1                      WEATHER_IN
+
 typedef const struct main_state_interface * (*main_state_get_t)(void);
 static const main_state_get_t (mode_selection_funs) = main_state_mode_selection_get;
 
 char test_buf[80];
 
-static void reset_test_buf(void)
-{
-    memset(test_buf, 0, 80);
-}
+static void reset_test_buf(void);
+static void init_state(void);
+static void entry_state(void);
+static void exit_state(void);
+static enum state_status press_ok_mode_button(void);
+static void press_up_button(void);
 
 TEST_GROUP(mode_selection);
 
 TEST_SETUP(mode_selection)
 {
     /* Init before every test */
-    mode_selection_funs() -> init(get_display_mock_funs());
+    /* Call state init and reset buffers */
+    init_state();
     display_mock_reset_buf();
     reset_test_buf();
 }
+
 
 TEST_TEAR_DOWN(mode_selection)
 {
     /* Cleanup after every test */
 }
 
+
 TEST(mode_selection, OnEntryDisplayOutput)
 {
-    mode_selection_funs() -> on_entry();
-    display_mock_read_buf(test_buf);
-    TEST_ASSERT_EQUAL_STRING("> 1. weather indoor\n  2. weather outdoor\n  3. set time\n  4. set alarm", test_buf);
+    entry_state();
+    TEST_ASSERT_EQUAL_STRING(SELECTED_MODE_1_MESSAGE, test_buf);
 }
+
 
 TEST(mode_selection, OnExitDisplayOutput)
 {
-    mode_selection_funs() -> on_entry();
-    mode_selection_funs() -> on_exit();
-    display_mock_read_buf(test_buf);
-    TEST_ASSERT_EQUAL_STRING("\0", test_buf);
+    entry_state();
+    exit_state();
+    TEST_ASSERT_EQUAL_STRING(CLEARED_MESSAGE, test_buf);
 }
+
 
 TEST(mode_selection, WhenSelectedModeIs1ClickUpButtonThenSelectedModeIsStill1)
 {
-    mode_selection_funs() -> on_entry();
-    mode_selection_funs() -> on_up_button_pressed();
-    display_mock_read_buf(test_buf);
-    TEST_ASSERT_EQUAL_STRING("> 1. weather indoor\n  2. weather outdoor\n  3. set time\n  4. set alarm", test_buf);
+    entry_state();
+    press_up_button();
+    enum state_status state = press_ok_mode_button();
 
-    enum state_status state = mode_selection_funs() -> on_ok_mode_button_pressed();
-    TEST_ASSERT_EQUAL(state, WEATHER_IN);
+    TEST_ASSERT_EQUAL_STRING(SELECTED_MODE_1_MESSAGE, test_buf);
+    TEST_ASSERT_EQUAL(state, MODE_1);
 }
 
 
+/* Helper functions */
+static void reset_test_buf(void)
+{
+    memset(test_buf, 0, 80);
+}
+
+
+static void init_state(void)
+{
+    mode_selection_funs() -> init(get_display_mock_funs());
+}
+
+
+static void entry_state(void)
+{
+    mode_selection_funs() -> on_entry();
+    display_mock_read_buf(test_buf);
+}
+
+
+static void exit_state(void)
+{
+    mode_selection_funs() -> on_exit();
+    display_mock_read_buf(test_buf);
+}
+
+
+static enum state_status press_ok_mode_button(void)
+{
+    return mode_selection_funs() -> on_ok_mode_button_pressed();
+}
+
+
+static void press_up_button(void)
+{
+    mode_selection_funs() -> on_up_button_pressed();
+    display_mock_read_buf(test_buf);
+}
+
+
+/* For correct compilation */
 void safe_state(void)
 {
     while (1);
