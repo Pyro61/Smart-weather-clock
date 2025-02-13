a9615b8df990de897eaaf33d60133772add4a3e9
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-02-13 21:39:56 +0100

    time: add function prototypes
    needed in weather in/out state handling

---
+++ Src/time/time.c
@@ -0,0 +1,17 @@
+#include "time.h"
+
+void time_init(void)
+{
+    //subscribe to EVENT_1S_ELAPSED
+    //nofity subscribers 1s elapsed callback to rtc_start_1s_timer
+}
+
+void time_set(struct time time)
+{
+
+}
+
+struct time time_get(void)
+{
+    
+}
---
+++ Src/time/time.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../types.h"
+
+
+void time_init(void);
+void time_set(struct time time);
+struct time time_get(void);
