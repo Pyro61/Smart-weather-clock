1531b4d713b8c034931f5dc0717983bd58a2d769
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-02-13 21:40:51 +0100

    adapter sys comm: add copying recv buffer to given buffer function

--- Src/sys_comm/adapter_sys_comm.h
+++ Src/sys_comm/adapter_sys_comm.h
@@ -2,6 +2,7 @@
 
 #include "../types.h"
 #include "sys_comm_hw_interface.h"
+#include <stdint.h>
 
 
 void adapter_sys_comm_init(struct sys_comm_hw_funs *funs);
@@ -9,3 +10,4 @@ void adapter_sys_comm_send_resp_data(struct weather_data *data);
 void adapter_sys_comm_send_req_start_meas(void);
 void adapter_sys_comm_send_req_stop_meas(void);
 void adapter_sys_comm_send_req_get_data(void);
+void adapter_sys_comm_copy_recv_buf(uint8_t *dest_buf);
